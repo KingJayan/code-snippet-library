7ab44c9e315172efd9ca4f6b2cc395d320afc5ab
jayanp <jayanp0202@gmail.com>
2026-03-05 21:43:33 -0600

    fix: suppress hydration warning from extensions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,7 @@ export default function RootLayout({
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <TooltipProvider delayDuration={200}>
           {children}
